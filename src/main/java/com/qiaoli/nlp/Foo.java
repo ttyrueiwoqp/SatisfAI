dd900249e823cd549c9cfad78257f6eac9728630
ttyrueiwoqp <lufangjiannus@gmail.com>
2017-04-19 06:30:52 +0800

    Config Language API

---
+++ src/main/java/com/qiaoli/nlp/Foo.java
@@ -0,0 +1,15 @@
+package com.qiaoli.nlp;
+
+/**
+ * Created by lvfan on 4/17/2017.
+ */
+public class Foo {
+
+    int x;
+    String y;
+
+    public Foo(int x, String y) {
+        this.x = x;
+        this.y = y;
+    }
+}
--- src/main/java/com/qiaoli/nlp/NLPController.java
+++ src/main/java/com/qiaoli/nlp/NLPController.java
@@ -1,9 +1,15 @@
 package com.qiaoli.nlp;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.google.cloud.language.spi.v1.LanguageServiceClient;
+
+import com.google.cloud.language.v1.AnalyzeSentimentResponse;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.Sentiment;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
 
 @RestController
 public class NLPController {
@@ -15,8 +21,38 @@ public class NLPController {
 
     @GetMapping("/analyse/{text}")
     public String analyse(@PathVariable String text) {
-        System.out.println(text + text);
-        return text + text;
+
+        // Instantiates a client
+        Sentiment sentiment;
+        StringBuilder sb;
+        try {
+            LanguageServiceClient language = LanguageServiceClient.create();
+            // The text to analyze
+            Document doc = Document.newBuilder()
+                    .setContent(text).setType(Type.PLAIN_TEXT).build();
+
+            // Detects the sentiment of the text
+            AnalyzeSentimentResponse dummy = language.analyzeSentiment(doc);
+
+            sentiment = dummy.getDocumentSentiment();
+
+            System.out.println(text + text);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "Error";
+        }
+
+        sb = new StringBuilder();
+        sb.append("Text: ").append(text).append("\n");
+        sb.append("Sentiment: ").append(sentiment.getScore()).append(sentiment.getMagnitude());
+
+        return sb.toString();
+    }
+
+    @GetMapping("/test/{text}")
+    public @ResponseBody
+    Foo test(@PathVariable String text) {
+        return new Foo(1, text);
     }
 
     /**
